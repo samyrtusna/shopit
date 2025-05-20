e4c8ae1f2022c66090501b2d0d0141c16da0a744
samir <samyrtusna@gmail.com>
2025-05-20 18:21:47 +0100

    ignore informations folder

--- frontend/src/components/Cart/Checkout.jsx
+++ frontend/src/components/Cart/Checkout.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import PaypalButton from "./PaypalButton";
 import { useDispatch, useSelector } from "react-redux";
+import { createCheckout } from "../../redux/slices/checkoutSlice";
 
 const Checkout = () => {
   const navigate = useNavigate();
